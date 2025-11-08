66de4d7d2c514d2a54933c351acaea0f7e792585
Salauddin Ahmed <110757660+Salauddin2k3@users.noreply.github.com>
2025-11-09 00:12:00 +0600

    Rest And Spread

--- src/first.ts
+++ src/first.ts
@@ -18,6 +18,28 @@ const arr : number[] = [2, 3, 4, 5, 6, 7];
 
 const squireArr : number[] = arr.map((element : number) => element * element);
 
-console.log("Arr Is:", arr, " : ", "Squire Array is:", squireArr);
+// console.log("Arr Is:", arr, " : ", "Squire Array is:", squireArr);
+
+
+//* Spread Operator
+
+const friend1 : string[] = ['Abul', 'Kabul', 'Babul'];
+
+const friend2 : string[] = ['Hablu', 'Bablu', 'Dablu'];
+
+const friend3 : string[] = ['Kodu', 'Modhu', 'Jodu'];
+
+const friendsSpread = [...friend1, ...friend2, ...friend3];
+
+// console.log(friendsSpread);
+
+
+//* Rest operator
+
+const restOperator = (...friends : string[]) => {
+    friends.forEach((friend : string) => console.log(friend))
+};
+
+restOperator('Abul', "dabul")
 
 
